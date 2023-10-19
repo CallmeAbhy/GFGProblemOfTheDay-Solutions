581ab7bcf6a044578f1c57990eb5fc1714b88700
Abhay Hitendra Dusane <99988396+CallmeAbhy@users.noreply.github.com>
2023-10-19 21:48:26 +0530

    Create Solution.cpp

---
+++ 19 Oct/Solution.cpp
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+	public:
+	//Function to find the level of node X.
+	int nodeLevel(int V, vector<int> adj[], int X) 
+	{
+	    // code here
+	    vector<bool> visited(V, false);
+        vector<int> level(V, 0);
+        queue<int> q;
+        
+        q.push(0);  // Start from node 0
+        visited[0] = true;
+
+        while (!q.empty()) 
+        {
+            int u = q.front();
+            q.pop();
+
+            for (int v : adj[u]) 
+            {
+                if (!visited[v]) 
+                {
+                    level[v] = level[u] + 1;
+                    visited[v] = true;
+                    q.push(v);
+
+                    if (v == X) 
+                    {
+                        return level[v];
+                    }
+                }
+            }
+        }
+        
+        return -1; // X not found
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E, X;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    		adj[v].push_back(u);
+    	}
+    	cin>>X;
+
+    	Solution obj;
+    	cout << obj.nodeLevel(V, adj, X) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
